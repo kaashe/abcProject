df5a75a369932e3156da43d62e0a7fb50d33da63
Kashif Ullah <kashifullah.dev@gmail.com>
2024-07-04 13:40:31 +0500

    fix

--- src/features/user/LandingIntro.js
+++ src/features/user/LandingIntro.js
@@ -5,15 +5,15 @@ function LandingIntro() {
     <div className="hero min-h-full rounded-l-xl bg-base-200">
       <div className="hero-content py-12">
         <div className="max-w-md">
-          <h1 className="text-3xl text-center font-bold ">
-            <div className="avatar placeholder">
-              <div className="bg-neutral text-neutral-content rounded-full w-10">
-                <span className="text-2xl">A</span>
-              </div>
-            </div>
-            BC
-          </h1>
-          <TemplatePointers />
+          <h1 className="text-3xl text-center font-bold ">User Portal</h1>
+          {/* <TemplatePointers /> */}
+          <div className="text-center mt-12">
+            <img
+              src="./AH1.png"
+              alt="User Dashboard"
+              className="w-48 inline-block rounded-full"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
