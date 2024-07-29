a6c797ebca6665ed1f7928d089fe793e41a17c49
Abdarkhan1 <129251163+Abdarkhan@users.noreply.github.com>
2024-07-29 15:51:59 +0500

    Lgog Updated

--- src/containers/LeftSidebar.js
+++ src/containers/LeftSidebar.js
@@ -1,62 +1,68 @@
-import routes from '../routes/sidebar'
-import { NavLink, Link, useLocation } from 'react-router-dom'
-import SidebarSubmenu from './SidebarSubmenu';
-import XMarkIcon from '@heroicons/react/24/outline/XMarkIcon'
+import routes from "../routes/sidebar";
+import { NavLink, Link, useLocation } from "react-router-dom";
+import SidebarSubmenu from "./SidebarSubmenu";
+import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
 
 function LeftSidebar() {
-    const location = useLocation();
+  const location = useLocation();
 
-    const close = (e) => {
-        document.getElementById('left-sidebar-drawer').click()
-    }
+  const close = (e) => {
+    document.getElementById("left-sidebar-drawer").click();
+  };
 
-    return (
-        <div className="drawer-side  z-30  ">
-            <label htmlFor="left-sidebar-drawer" className="drawer-overlay"></label>
-            <ul className="menu  pt-2 w-60 bg-base-100 min-h-full   text-base-content">
-                <button className="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden" onClick={() => close()}>
-                    <XMarkIcon className="h-5 inline-block w-5" />
-                </button>
+  return (
+    <div className="drawer-side  z-30  ">
+      <label htmlFor="left-sidebar-drawer" className="drawer-overlay"></label>
+      <ul className="menu  pt-2 w-60 bg-base-100 min-h-full   text-base-content">
+        <button
+          className="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden"
+          onClick={() => close()}
+        >
+          <XMarkIcon className="h-5 inline-block w-5" />
+        </button>
 
-                <li className="mb-2 font-semibold text-xl">
+        <li className="mb-2 font-semibold text-xl">
+          <Link to={"/app/dashboard"}>
+            <img src="/AH2.png" width="90" />
 
-                    <Link to={"/app/dashboard"}>
-                        <img
+            {/* <img
                             className="mask mask-squircle w-10"
                             src="/AH1.png"
                             alt="AH Logo"
                         />
-                        Arkin Hype
-                    </Link>
-
-                </li>
-                {
-                    routes.map((route, k) => {
-                        return (
-                            <li className="" key={k}>
-                                {
-                                    route.submenu ?
-                                        <SidebarSubmenu {...route} /> :
-                                        (<NavLink
-                                            end
-                                            to={route.path}
-                                            className={({ isActive }) => `${isActive ? 'font-semibold  bg-base-200 ' : 'font-normal'}`} >
-                                            {route.icon} {route.name}
-                                            {
-                                                location.pathname === route.path ? (<span className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary "
-                                                    aria-hidden="true"></span>) : null
-                                            }
-                                        </NavLink>)
-                                }
-
-                            </li>
-                        )
-                    })
-                }
-
-            </ul>
-        </div>
-    )
+                        Arkin Hype */}
+          </Link>
+        </li>
+        {routes.map((route, k) => {
+          return (
+            <li className="" key={k}>
+              {route.submenu ? (
+                <SidebarSubmenu {...route} />
+              ) : (
+                <NavLink
+                  end
+                  to={route.path}
+                  className={({ isActive }) =>
+                    `${
+                      isActive ? "font-semibold  bg-base-200 " : "font-normal"
+                    }`
+                  }
+                >
+                  {route.icon} {route.name}
+                  {location.pathname === route.path ? (
+                    <span
+                      className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary "
+                      aria-hidden="true"
+                    ></span>
+                  ) : null}
+                </NavLink>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default LeftSidebar
+export default LeftSidebar;
--- src/features/user/LandingIntro.js
+++ src/features/user/LandingIntro.js
@@ -4,17 +4,19 @@ function LandingIntro() {
   return (
     <div className="hero min-h-full rounded-l-xl bg-base-200">
       <div className="hero-content py-12">
-        <div className="max-w-md">
-          <h1 className="text-3xl text-center font-bold ">User Portal</h1>
+      <img src="/logo.jpeg" />
+
+        {/* <div className="max-w-md">
+          <h1 className="text-3xl text-center font-bold ">User Portal</h1> */}
           {/* <TemplatePointers /> */}
-          <div className="text-center mt-12">
+          {/* <div className="text-center mt-12">
             <img
               src="./AH1.png"
               alt="User Dashboard"
               className="w-48 inline-block rounded-full"
             ></img>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
